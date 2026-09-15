b3714a2c49bfcdfac222ed7fcde39b7a1f7c11b8
shirlymontes <lr.srmontes@mmdc.mcl.edu.ph>
2026-09-15 21:31:21 +0800

    chore: merge team staging with complete auth from origin main

---
+++ backend/src/models/CourseAlias.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose');  const courseAliasSchema = new mongoose.Schema({   canonicalName: {     type: String,     required: true,     unique: true   },   aliases: {     type: [String],     default: []   } });  module.exports = mongoose.model('CourseAlias', courseAliasSchema);
